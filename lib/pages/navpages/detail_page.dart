18437e5ad9f3da8e6aa74b7c47718f79d92b1938
vikas chouhan <iamvikki0@gmail.com>
2022-01-11 18:00:51 +0530

    detail page

---
+++ lib/pages/navpages/detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(),
+      ),
+    );
+  }
+}
